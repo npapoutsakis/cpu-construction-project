5f47c252302b1626be21081ede99dc162d63c5e4
Nikolaos Papoutsakis <61113978+npapoutsakis@users.noreply.github.com>
2022-03-24 01:37:10 +0200

    Update ALU.vhd

--- Phase 1/ALU.vhd
+++ Phase 1/ALU.vhd
@@ -92,27 +92,28 @@ begin
 		
 		end if;
 	end process;
-	
-	
+		
 	Output <= addALU(31 downto 0) after 10ns when Op = "0000" else
-			  subALU(31 downto 0) after 10ns when Op = "0001" else
-			  early_output after 10ns;	
+	          subALU(31 downto 0) after 10ns when Op = "0001" else
+		  early_output after 10ns;	
 	
+		
 	early_ovf <= '1' when ((inputA(31) = inputB(31)) and (addALU(31) /= inputA(31)) and (Op = "0000")) else 
-				 '1' when ((inputA(31) /= inputB(31)) and (subALU(31) = inputB(31)) and (Op = "0001")) else
-				 '0';
+		     '1' when ((inputA(31) /= inputB(31)) and (subALU(31) = inputB(31)) and (Op = "0001")) else
+		     '0';
 	Ovf <= early_ovf after 10ns;
 	
+		
 	early_cout <= addALU(32) when Op = "0000" and early_ovf /= '1' else
-			      subALU(32) when Op = "0001" and early_ovf /= '1' else
-			      '0';
+		      subALU(32) when Op = "0001" and early_ovf /= '1' else
+		      '0';
 	Cout <= early_cout after 10ns;
 	
+		
 	early_zero <= '1' when addALU(31 downto 0)= 0 and Op = "0000" else
-				  '1' when subALU(31 downto 0)= 0 and Op = "0001" else
-				  '1' when early_output = 0 and Op /= "0000" and Op /= "0001" else
-				  '0';
-	
+		      '1' when subALU(31 downto 0)= 0 and Op = "0001" else
+		      '1' when early_output = 0 and Op /= "0000" and Op /= "0001" else
+	              '0';
 	Zero <= early_zero after 10ns;
 	
 end Behavioral;
